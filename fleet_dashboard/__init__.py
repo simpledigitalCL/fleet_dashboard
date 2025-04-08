35d2a5163803074d195ae8db83c3854ad9848c57
Simpledigital <admin@simpledigital.cl>
2025-04-08 19:04:14 -0400

    Update __init__.py

--- fleet_dashboard/__init__.py
+++ fleet_dashboard/__init__.py
@@ -1,6 +1,4 @@
-def pre_init_check(cr):
-    from odoo import api, SUPERUSER_ID
-    env = api.Environment(cr, SUPERUSER_ID, {})
+def pre_init_check(env):
     if not env.registry.get('stock.picking'):
         raise Exception("El módulo 'stock' no está cargado. Instálalo antes de 'fleet_dashboard'.")
 
