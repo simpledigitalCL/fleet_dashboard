9ad18f3f360032387d18c9da30da646755a31ae2
Simpledigital <admin@simpledigital.cl>
2025-04-08 23:00:53 -0400

    Update __init__.py

--- fleet_dashboard/__init__.py
+++ fleet_dashboard/__init__.py
@@ -1,5 +1,1 @@
-def pre_init_check(env):
-    if not env.registry.get('stock.picking'):
-        raise Exception("El módulo 'stock' no está cargado. Instálalo antes de 'fleet_dashboard'.")
-
 from . import models
